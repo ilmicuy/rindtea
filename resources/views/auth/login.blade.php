072fb123deeec62696738b091dcdcb5a6cdf6ed9
Agy Nurwicaksono <agy@agyson.com>
2024-08-12 00:53:12 +0700

    feat: Unhide and show password on login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,8 +10,11 @@
                         autocomplete="username" placeholder="Your Email" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
 
-                    <x-text-input id="password" class="login-input" type="password" name="password"
-                        autocomplete="current-password" placeholder="Your Password" />
+                    <div class="password-container" style="display: flex; align-items: center;">
+                        <x-text-input id="password" class="login-input" type="password" name="password"
+                            autocomplete="current-password" placeholder="Your Password" style="flex: 1;" />
+                        <span id="togglePassword" class="toggle-password" style="cursor: pointer; margin-left: 10px;">Show</span>
+                    </div>
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
 
                     <button class="login-button" type="submit">Submit</button>
@@ -19,4 +22,20 @@
             </div>
         </div>
     </section>
+
+    @push('myscript')
+    <script>
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+
+        togglePassword.addEventListener('click', function (e) {
+            // toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+
+            // toggle the text
+            this.textContent = this.textContent === 'Show' ? 'Hide' : 'Show';
+        });
+    </script>
+    @endpush
 @endsection
