effe09b57eaa103a2dcd0e2cf1783d3fdd3a63aa
Agy Nurwicaksono <agy@agyson.com>
2024-08-12 08:58:37 +0700

    fixing X-CSRF

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -206,7 +206,7 @@
     $(function() {
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token']").attr('content')
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
 
